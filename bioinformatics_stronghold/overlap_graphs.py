297c48c74e0899a869b9971a33374b49c41beae0
Fantasy Dekyi <fantasy.dekyi@proton.me>
2024-05-30 17:21:31 -0700

    finished graphing problem

--- bioinformatics_stronghold/overlap_graphs.py
+++ bioinformatics_stronghold/overlap_graphs.py
@@ -13,26 +13,23 @@ dataset = dataset_file.read()
 split_dataset = list(filter(lambda x: x != '', dataset.split('>')))
 
 dna_strings = {}
-dna_string_length = 0
+dna_string_labels = []
 x_fix_length = 3
 adjacency_list = set()
 for dna_string in split_dataset:
     (dna_data_label, dna_data_string) = parse_dna_string(dna_string)
     dna_strings[dna_data_label] = dna_data_string
-    dna_string_length = len(dna_data_string)
+    dna_string_labels.append(dna_data_label)
 
-for dna_string_a in dna_strings:
-    prefix = dna_strings[dna_string_a][0:x_fix_length]
-    for dna_string_b in dna_strings:
+for dna_string_a in dna_string_labels:
+    current_string = dna_strings[dna_string_a]
+    suffix = current_string[len(current_string) - x_fix_length: len(current_string)]
+    for dna_string_b in dna_string_labels:
         if dna_string_a == dna_string_b or dna_strings[dna_string_a] == dna_strings[dna_string_b]:
             continue
 
-        current_string = dna_strings[dna_string_b]
-        suffix = current_string[len(current_string) - x_fix_length: len(current_string)]
+        prefix = dna_strings[dna_string_b][0:x_fix_length]
         if prefix == suffix:
-            if dna_string_a < dna_string_b:
-                adjacency_list.add(dna_string_a + ' ' + dna_string_b)
-            else:
-                adjacency_list.add(dna_string_b + ' ' + dna_string_a)
+            adjacency_list.add(dna_string_a + ' ' + dna_string_b)
 
 print('\n'.join(adjacency_list))
